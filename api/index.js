6864e56bf7dae5107252a063f4e63a1b1f04899e
Emiliano Rodriguez <emilianor81@gmail.com>
2021-09-02 10:54:49 -0300

    complete

--- api/index.js
+++ api/index.js
@@ -2,90 +2,90 @@ const server = require('./src/app.js');
 const { conn, Deposito } = require('./src/db.js');
 
 // Syncing all the models at once.
-conn.sync({ force: true }).then(() => {
+conn.sync({ force: false }).then(() => {
   server.listen(3001, () => {
     console.log('%s listening at 3001'); // eslint-disable-line no-console
-    const WH01 = Deposito.create({
-      name: 'Buenos Aires',
-      id: 1,
-      limite:200,
-      stock: 0,
-      direccion: 'C1063 CABA'
-    });
-    const WH02 = Deposito.create({
-      name: 'Rosario',
-      id: 2,
-      limite: 70,
-      stock: 0,
-      direccion: 'S2000 Rosario, Santa Fe'
+    // const WH01 = Deposito.create({
+    //   name: 'Buenos Aires',
+    //   id: 1,
+    //   limite:200,
+    //   stock: 0,
+    //   direccion: 'C1063 CABA'
+    // });
+    // const WH02 = Deposito.create({
+    //   name: 'Rosario',
+    //   id: 2,
+    //   limite: 70,
+    //   stock: 0,
+    //   direccion: 'S2000 Rosario, Santa Fe'
 
-    });
-    const WH03 = Deposito.create({
-      name: 'Córdoba',
-      id: 3,
-      limite:150,
-      stock: 0,
-      direccion: 'X5000JJD Cordoba, Argentina'
+    // });
+    // const WH03 = Deposito.create({
+    //   name: 'Córdoba',
+    //   id: 3,
+    //   limite:150,
+    //   stock: 0,
+    //   direccion: 'X5000JJD Cordoba, Argentina'
 
-    });
-    const WH04 = Deposito.create({
-      name: 'Trelew',
-      id: 4,
-      limite:140,
-      stock: 0,
+    // });
+    // const WH04 = Deposito.create({
+    //   name: 'Trelew',
+    //   id: 4,
+    //   limite:140,
+    //   stock: 0,
 
-      direccion: 'Rivadavia 390, Trelew, Chubut'
+    //   direccion: 'Rivadavia 390, Trelew, Chubut'
 
-    });
-    const WH05 = Deposito.create({
-      name: 'Mendoza',
-      id: 5,
-      limite: 150,
-      stock: 0,
-      direccion: 'Gral. Espejo 300, M5502 AVJ, Mendoza'
+    // });
+    // const WH05 = Deposito.create({
+    //   name: 'Mendoza',
+    //   id: 5,
+    //   limite: 150,
+    //   stock: 0,
+    //   direccion: 'Gral. Espejo 300, M5502 AVJ, Mendoza'
 
-    });
-    const WH06 = Deposito.create({
-      name: 'La Plata',
-      id: 6,
-      limite: 100,
-      stock: 0,
-      direccion: 'B1900 La Plata, Provincia de Buenos Aires'
+    // });
+    // const WH06 = Deposito.create({
+    //   name: 'La Plata',
+    //   id: 6,
+    //   limite: 100,
+    //   stock: 0,
+    //   direccion: 'B1900 La Plata, Provincia de Buenos Aires'
 
-    });
-    const WH07 = Deposito.create({
-      name: 'San Miguel de Tucumán',
-      id: 7,
-      limite:120,
-      stock: 0,
+    // });
+    // const WH07 = Deposito.create({
+    //   name: 'San Miguel de Tucumán',
+    //   id: 7,
+    //   limite:120,
+    //   stock: 0,
 
-      direccion: 'T4000 San Miguel de Tucuman, Tucuman'
-    });
-    const WH08 = Deposito.create({
-      name: 'Mar del Plata',
-      id: 8,
-      limite:180,
-      stock: 0,
-      direccion: 'Mar del Plata, Provincia de Buenos Aires'
-    });
-    const WH09 = Deposito.create({
-      name: 'Salta',
-      id: 9,
-      limite:140,
-      stock: 0,
+    //   direccion: 'T4000 San Miguel de Tucuman, Tucuman'
+    // });
+    // const WH08 = Deposito.create({
+    //   name: 'Mar del Plata',
+    //   id: 8,
+    //   limite:180,
+    //   stock: 0,
+    //   direccion: 'Mar del Plata, Provincia de Buenos Aires'
+    // });
+    // const WH09 = Deposito.create({
+    //   name: 'Salta',
+    //   id: 9,
+    //   limite:140,
+    //   stock: 0,
 
-      direccion: 'Caseros 500, Salta'
-    });
-    const WH10 = Deposito.create({
-      name: 'Santa Fe',
-      id: 10,
-      limite: 70,
-      stock: 0,
+    //   direccion: 'Caseros 500, Salta'
+    // });
+    // const WH10 = Deposito.create({
+    //   name: 'Santa Fe',
+    //   id: 10,
+    //   limite: 70,
+    //   stock: 0,
 
-      direccion: 'provincia de santa fe, Argentina'
-    });
-    Promise.all([WH01, WH02, WH03, WH04,
-      WH05, WH06, WH07, WH08, WH09, WH10])
-      .then(res => console.log('Depositos created'))
+    //   direccion: 'provincia de santa fe, Argentina'
+    // });
+    // Promise.all([WH01, WH02, WH03, WH04,
+    //   WH05, WH06, WH07, WH08, WH09, WH10])
+    //   .then(res => console.log('Depositos created'))
   });
 });
--- api/src/routes/deposito.js
+++ api/src/routes/deposito.js
@@ -12,8 +12,10 @@ const router = Router();
 
 const addShipping = router.post('/', async (req, res)=>{
    const {destino}=req.body
+   //declaro el array que me sirvara para cargar los datos que necesito de cada deposito para poder hacer los calculos
    let ArrayDepositosDistancias = [];
     try{
+        //me traigo los datos de la tabla depositos
         const depositos = await Deposito.findAll();
         for(i = 0; i < depositos.length ; i++){
             let direccion = depositos[i].dataValues.direccion
@@ -21,20 +23,52 @@ const addShipping = router.post('/', async (req, res)=>{
             let limite = depositos[i].dataValues.limite
             let id = depositos[i].dataValues.id
             let stock = depositos[i].dataValues.stock
-        //     //hacer un string concat con todas las direcciones de depositos y | para hacer una sola llamada a la api
-        // let direccion = direccion1 + '|'
-        //     //calculo la distancia entre el destino del paquete y cada uno de los depositos y lo guardo en un array para poder tener los datos que necesito
-         const distanciaADeposito = await axios.get(`https://maps.googleapis.com/maps/api/distancematrix/json?key=${API_KEY}&destinations=${destino}&origins=${direccion}`);
-         let distancia = distanciaADeposito.data.rows[0].elements[0].distance.text; 
-         distancia = parseInt(distancia.split(''));
-         console.log(distancia)
-         const NuevaDistancia = {id, nombre, limite, direccion, stock, distancia };
-         ArrayDepositosDistancias.push(NuevaDistancia)
+            //hacer un string concat con todas las direcciones de depositos y | para hacer una sola llamada a la api
+            // let direccion = direccion1 + '|'
+            //calculo la distancia entre el destino del paquete y cada uno de los depositos y lo guardo en un array para poder tener los datos que necesito
+            const distanciaADeposito = await axios.get(`https://maps.googleapis.com/maps/api/distancematrix/json?key=${API_KEY}&destinations=${destino}&origins=${direccion}`);
+            let distancia = distanciaADeposito.data.rows[0].elements[0].distance.text; 
+            distancia = distancia.split(' ')
+            distancia = distancia[0];
+            distancia = distancia.replace(/,/g, '')
+            distancia = parseInt(distancia)
+            //Cargo todos los datos que necesito en el array, incluida la distancia entre cada deposito y el destino del paquete
+            const NuevaDistancia = {id, nombre, limite, direccion, stock, distancia };
+            ArrayDepositosDistancias.push(NuevaDistancia)
           }
-          ArrayDepositosDistancias.sort((a, b) => {return a.distancia - b.distancia}); // --> 3, 12, 23
-          console.log(ArrayDepositosDistancias)
-          //UNA VEZ QUE TENGO TODO ORDENADO POR DISTANCIA, RECORRO Y PREGUNTO SI TIENE MENOS DEL 95% OCUPACION
-        ArrayDepositosDistancias?res.send('hola'):res.send('no se encontro la localidad de destino')
+          //ordeno el array en forma ascendente por la distancia el deposito hasta el destino del paquete
+          ArrayDepositosDistancias.sort((a, b) => {return a.distancia - b.distancia}); 
+          //Recorro el array para ver en que deposito me conviene enviar el paquete
+          for(let i = 0 ; i < ArrayDepositosDistancias.length ; i++){
+              //calculo la variable para la alerta, para ver si esta disponible ese deposito o tengo que enviar a otro, o pagar la multa
+              let alerta = Math.ceil(ArrayDepositosDistancias[i].limite * 0.95)
+              //compruebo si cada uno de los depositos tiene espacio para un paquete mas, recorriendo de acuerdo la distancia mas optima
+              if(ArrayDepositosDistancias[i].stock < alerta){
+                  // si entra a este if automaticamente lo guardo ahi y corto la ejecucion, previo actualizar el stock
+                  let id = ArrayDepositosDistancias[i].id
+                  let stock = ArrayDepositosDistancias[i].stock
+                const depositoAmodificar = await Deposito.findByPk(id)
+                await depositoAmodificar.update({stock: stock + 1})
+                 return res.json(ArrayDepositosDistancias[i])
+              }else{
+                  if (ArrayDepositosDistancias[i+1].distancia > 350){
+                    //este es el caso que me conviene pagar la multa
+                    let id1 = ArrayDepositosDistancias[i].id
+                    let stock = ArrayDepositosDistancias[i].stock
+                    const depositoAmodificar = await Deposito.findByPk(id1)
+                    await depositoAmodificar.update({stock: stock + 1})
+                    return res.json(ArrayDepositosDistancias[i+1])
+
+                  }else{
+                    //en este caso me conviene enviar al mas cercano
+                    let ids2 = ArrayDepositosDistancias[i+1].id
+                    let stock = ArrayDepositosDistancias[i+1].stock
+                  const depositoAmodificar = await Deposito.findByPk(ids2)
+                  await depositoAmodificar.update({stock: stock + 1})
+                   return res.json(ArrayDepositosDistancias[i])
+                  }
+              }
+          }
     }
     catch(err){
         res.send('err')
